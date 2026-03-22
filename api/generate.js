8a7ceb2279c29fba65139de93340414ed60a2d2a
frenchois2025 <frenchois2025@gmail.com>
2026-03-22 20:00:44 -0300

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -18,7 +18,7 @@ module.exports = async function handler(req, res) {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           contents: [{ parts: [{ text: prompt }] }],
-          generationConfig: { temperature: 0.7, maxOutputTokens: 2000, thinkingConfig: { thinkingBudget: 0 } }
+          generationConfig: { temperature: 0.7, maxOutputTokens: 4000, thinkingConfig: { thinkingBudget: 0 } }
         })
       }
     );
